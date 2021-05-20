24750d449223444fc732e7fd27649928b4deff22
Errorcrafter <69189354+Errorcrafter@users.noreply.github.com>
2021-05-20 15:33:15 +0800

    added risk assesment (kinda) and more randomness

--- code/exampleStrats/bongcloud-xianthu.py
+++ code/exampleStrats/bongcloud-xianthu.py
@@ -1,12 +1,30 @@
 ###############################
 #                             #
-#       BongCloud Strat       #
+#       Bongcloud Strat       #
 #  Written by Xianthu Laman   #
 #                             #
 ###############################
 
+# defect      > 0
+# cooperate   > 1
+
 import random
 import numpy as np
+
+def noise(choice,observations): # add additional randomness to screw with people
+    weighting = observations / 7
+    weighting = round(weighting, 3)
+
+    if random.random() > weighting: # if the random number falls abv the weighting.
+
+        if random.randint(1,10) == 1: # flips the choice at random
+            choice = not choice
+
+    if random.randint(1,(20*weighting)) == 20*weighting: # very small chance to attempt another flip
+        noise(choice,observations)
+    else:
+        return choice
+    
 
 def strategy(history,memory):
 
@@ -14,9 +32,16 @@ def strategy(history,memory):
     game_length = history.shape[1]
     choice = None
 
-    if game_length > 5: # observe the opponent's actions, choose randomly
+    if game_length > 7: # observe the opponent's actions, choose randomly
         choice = random.randint(0,1)
-    else:
+    else: # after observations
         opponent = history[1]
+        obvs = np.count_nonzero(opponent-1)
+        if obvs > 5: # low rate of cooperation
+            choice = history[1,-1] # Do Tit for Tat
+        elif obvs > 3: # middling rate of cooperation
+            choice = 1
+        else: # very high rate of cooperation, EXPLOIT TIME!!!!!
+            choice = 0
 
-    return choice,memory
+    return noise(choice,memory)
