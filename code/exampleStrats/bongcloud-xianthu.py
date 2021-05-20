ce2e914a534885d7bad51ed79c079b46bf8d01e9
Errorcrafter <69189354+Errorcrafter@users.noreply.github.com>
2021-05-20 17:45:32 +0800

    e

--- code/exampleStrats/bongcloud-xianthu.py
+++ code/exampleStrats/bongcloud-xianthu.py
@@ -14,7 +14,7 @@ import numpy as np
 def noise(choice,observations:int): # add additional randomness to screw with people
 
     try:
-        weighting = int(observations) / 7
+        weighting = observations / 7
     except Exception as e:
         print(f"ERROR: {str(e)}")
         return 0
@@ -27,7 +27,7 @@ def noise(choice,observations:int): # add additional randomness to screw with pe
             choice = not choice
 
     if random.randint(1,200) == 200: # very small chance to attempt another flip
-        noise(choice,observations)
+        return noise(choice,observations)
     else:
         return choice
     
@@ -39,20 +39,25 @@ def strategy(history,memory):
     choice = None
     obvs = None
 
-    if game_length <= 7 : # observe the opponent's actions, choose randomly
+    if game_length <= 5 : # observe the opponent's actions, choose randomly
         choice = random.randint(0,1)
 
     elif game_length <= 21: # after observations, phase 1 - pseudo-random (prolly broken)
         opponent = history[1]
         obvs = np.count_nonzero(opponent-1)
-        if obvs > 5: # low rate of cooperation
+        if obvs >= 4: # low rate of cooperation
             choice = history[1,-1] # Do Tit for Tat
-        elif obvs > 3: # middling rate of cooperation
+        elif obvs >= 2: # middling rate of cooperation
             choice = 0
         else: # very high rate of cooperation, EXPLOIT TIME!!!!!
             choice = 0
+        
+        if random.randint(1,3) == 3:
+            choice = noise(choice,obvs)
 
-    elif game_length <= 36: # phase 2 - grimTrigger?
+        memory = None
+
+    elif game_length <= 45: # phase 2 - grimTrigger?
         wronged = False
         if memory is not None and memory: # Has memory that it was already wronged.
             wronged = True
@@ -68,4 +73,4 @@ def strategy(history,memory):
     else: # final phase - always defect
         choice = 0
 
-    return noise(choice,obvs),memory
+    return choice,memory
