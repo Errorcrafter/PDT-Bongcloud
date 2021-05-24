a6de0e0477b3251db6840b6e6578702b1921ab35
Errorcrafter <69189354+Errorcrafter@users.noreply.github.com>
2021-05-24 18:06:10 +0800

    final version. its shit, but its the highest placement i have been able to get. may the best algo win

--- code/exampleStrats/smooth_brain-xianthu.py
+++ code/exampleStrats/smooth_brain-xianthu.py
@@ -1,10 +1,33 @@
 # SmoothBrain by Xianthu (macpherson#1415)
 
+
 import random
+
+
+def calc_income(history,rounds):
+    game_ln = history.shape[1]
+    if game_ln > rounds:
+        income = 0
+        for i in range(1,rounds):
+            if history[0,-i] == 0 and history[1,-1] == 0:  # if both defect
+                income += 1
+            elif history[0,-i] == 0 and history[1,-1] == 1:  # if you defect
+                income += 5
+            elif history[0,-i] == 1 and history[1,-1] == 0:  # if opponent defects
+                income += 0
+            elif history[0,-i] == 1 and history[1,-1] == 1:  # if both comply
+                income += 3
+            else:
+                return "Error!"
+    
+        return income
+
 
 def strategy(history,memory):
     
     game_ln = history.shape[1]
+
+
     if memory is None:  # on round start, create vars for memory
         defect_count = 0        # will be saved in memory[0]
         punish_timer = 0        # will be saved in memory[1]
@@ -19,38 +42,48 @@ def strategy(history,memory):
         punish_duration = memory[3]
         is_punishing = memory[4]
 
+
     if game_ln == 0:  # always comply on the first turn
         choice = 1
     else:
         punish_timer -= 1  # decrement the timer no matter what
 
         if history[1,-1] == 1:  # oh look, they are benefiting me!
-            #print("a")
             choice = 1
         else:  # hey no fair
-            #print("b")
+
+
             choice = 1  # forgive them if defect_count is below punish_threshold
             defect_count += 1  # increment
 
-            if defect_count >= punish_threshold and punish_timer < 1:  # start punishing them
-                #print("c")
+            if defect_count >= punish_threshold and punish_timer < 1:  # start punishing them when too many defects
                 punish_timer = punish_duration
-                is_punishing = True
+                is_punishing = True  # makes use of le punish timer
                 choice = 0
 
+
+            if game_ln> 10:
+                if calc_income(history,10) <= 10:  # always punish if opponent has defaulted to always defect
+                    is_punishing = True
+                    punish_timer = 999999999
+
+
         if is_punishing == True:
+
             if punish_timer > 1:
-                #print("dd")
                 choice = 0  # defect go brrrr
-            elif punish_timer == 1:
-                #print("e")
+
+            elif punish_timer == 1:  # when defect timer stops
+
                 choice = 0
                 is_punishing = False
-                if random.randint(0,1) > 0:  # 1 in 2 chance of becoming angrier
+                if random.randint(1,4) != 4:  # 3 in 4 chance of becoming angrier :troll:
                     punish_duration += 1
                     punish_threshold -= 1
+                defect_count = 0
             else:
                 pass
+
 
     return choice, [defect_count,punish_timer,punish_threshold,punish_duration,is_punishing]
             
